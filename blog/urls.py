dd25c5fb0344a7060306454425f4fa8ca28304e6
Vladimír Pokorný <vpokorny@fides.com>
2024-02-19 12:24:55 +0100

    #3 Generate QR paid code
    + change default viewe method to ministrant_detail method to pass data to the template

--- blog/urls.py
+++ blog/urls.py
@@ -14,7 +14,7 @@ from . import views
 urlpatterns = [
     path('', MinistrantListView.as_view(), name='blog-home'),
     path('user/<str:username>', UserMinistrantListView.as_view(), name='user-ministrants'),
-    path('ministrant/<int:pk>/', MinistrantDetailView.as_view(), name='ministrant-detail'),
+    path('ministrant/<int:pk>/', MinistrantDetailView.ministrant_detail, name='ministrant-detail'),
     path('ministrant/new/', MinistrantCreateView.as_view(), name='ministrant-create'),
     path('ministrant/<int:pk>/update/', MinistrantUpdateView.as_view(), name='ministrant-update'),
     path('ministrant/<int:pk>/delete/', MinistrantDeleteView.as_view(), name='ministrant-delete'),
