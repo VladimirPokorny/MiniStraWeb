489914d1c650f8b466505637ad2f85138de326fe
Vladimír Pokorný <vpokorny@fides.com>
2024-02-21 15:19:00 +0100

    #11 generating PDF with invoice + add debug prints

--- utils/invoice_generator.py
+++ utils/invoice_generator.py
@@ -23,17 +23,29 @@ class InvoiceGenerator:
 
         self.folder_name = f'{self.ministrant.unicode_name}'
         self.folder_path = rf'{settings.BASE_DIR}\media\invoices\{self.folder_name}'
-        self.latex_path = rf'{settings.BASE_DIR}\media\invoices\{self.folder_name}\{self.folder_name}.tex'
-        self.output_path = rf'{settings.BASE_DIR}\media\invoices\{self.folder_name}\{self.folder_name}.pdf'
+        self.latex_path = rf'{settings.BASE_DIR}\media\invoices\{self.folder_name}\{self.folder_name}_faktura.tex'
+        self.output_path = rf'{settings.BASE_DIR}\media\invoices\{self.folder_name}\{self.folder_name}_faktura.pdf'
+
+        print('\n\n\n\n\n')
+        print(self.folder_name)
 
         directory = os.path.dirname(self.latex_path)
         os.makedirs(directory, exist_ok=True)
 
-        latex_empty_source = r'C:\Programming\MiniStraWeb\utils\templates\invoice\invoice.tex'
+        latex_empty_source = os.path.join(settings.TEMPLATES[0]['DIRS'][1], 'invoice.tex')
+
+        print('\n\n\n\n\n')
+        print(latex_empty_source)
+
         latex_source = render_to_string(latex_empty_source, input_data)
+
+        print(latex_source)
         
         with open(self.latex_path, 'w') as file:
             file.write(latex_source)
+
+        print(self.latex_path)
+        print(self.folder_path)
 
         subprocess.run(['pdflatex', self.latex_path], cwd=self.folder_path)
         clean_latex_logs(self.folder_path)
