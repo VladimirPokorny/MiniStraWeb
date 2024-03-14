4ede7cc870d2496d40d1a88d6bdbec5c5c6a7f29
Vladimír Pokorný <vpokorny@fides.com>
2024-03-14 16:12:29 +0100

    QR code generator logic outside models.py #13
    + sorts imports
    + add docstrings

--- blog/models.py
+++ blog/models.py
@@ -1,18 +1,15 @@
-from django.db import models
-from django.utils import timezone
-from django.contrib.auth.models import User
-from django.urls import reverse
-import qrcode
 import os
-from django.conf import settings
-from io import BytesIO
-from django.core.files.base import ContentFile
-from unidecode import unidecode
-from utils.qr_pay_generator import QRPayGenerator
 
+from django.contrib.auth.models import User
 from django.core.validators import RegexValidator
-from camp.models import BankAccount, SummerCampInfo
-from PIL import Image as PIL
+from django.db import models
+from django.urls import reverse
+from django.utils import timezone
+
+from unidecode import unidecode
+
+from camp.models import SummerCampInfo
+from utils.qr_pay_generator import QRPayGenerator
 
 
 INSURANCE_COMPANIES = [
@@ -27,6 +24,9 @@ INSURANCE_COMPANIES = [
 
 
 class PhoneField(models.CharField):
+    '''
+    A PhoneField class is a custom model field for phone numbers.      
+    '''
     phone_regex = RegexValidator(
         regex=r'^\+(?:\d{3}\s?){4}$',
         message="Telefonní číslo musí být zadáno ve formátu: '+420 999 999 999'."
@@ -39,6 +39,9 @@ class PhoneField(models.CharField):
 
 
 class Ministrant(models.Model):
+    '''
+    A Ministrant class represents a person who is going to attend the summer camp.
+    '''
     time_stamp = models.DateTimeField(default=timezone.now)
     birthname = models.CharField(max_length=100, blank=True)
     surname = models.CharField(max_length=100, blank=True)
@@ -67,34 +70,56 @@ class Ministrant(models.Model):
     unicode_name = models.CharField(max_length=100, blank=False)
 
     def __str__(self) -> str:
-        """
+        '''
         Returns a string representation of the object containing the surname and birth name.
-
-        :return: A string representing the object.
-        """
+        
+        Returns
+        -------
+        str
+            A string representation of the object containing the surname and birth name.
+        '''
         return f'{self.surname} {self.birthname}'
-
+    
     def get_absolute_url(self) -> str:
-        """
-        Returns the absolute URL to access the detail view of a `Ministrant` instance.
+        '''
+        Returns the absolute URL of the object.
 
-        :param self: The `Ministrant` instance to generate the URL for.
-        :type self: Ministrant
-        :return: The absolute URL to access the detail view of the `Ministrant` instance.
-        :rtype: str
-        """
+        Returns
+        -------
+        str
+            The absolute URL of the object.
+        '''
         return reverse('ministrant-detail', kwargs={'pk': self.pk})
     
     @property
     def variable_symbol(self) -> str:
+        '''
+        Returns a variable symbol for the payment.
+        
+        Returns
+        -------
+        str
+            A variable symbol for the payment.
+        '''
         actual_camp_year = SummerCampInfo.objects.first().start_date.year
         return f'{actual_camp_year}{self.pk:04d}'
 
     @property
     def unicode_name(self) -> str:
+        '''
+        Returns the name of the object in a unicode format.
+
+        Returns
+        -------
+        str
+            The name of the object in a unicode format.
+        '''
         return unidecode(f'{self.surname}_{self.birthname}')
       
     def save(self, *args, **kwargs) -> None:
+        '''
+        Saves the object to the database.
+        '''
         super().save(*args, **kwargs)  # Call the "real" save() method.
 
         if not self.qr_pay_code:
